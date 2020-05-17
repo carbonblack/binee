010157a1a04d9c26a00cc4a4797f2369918674f4
mohamedahmedabdelmonim <muhammedabdelmonim@gmail.com>
2020-05-17 21:15:38 +0200

    (full hook) GetCurrentDirectory,FindExecutable

--- windows/shellapi.go
+++ windows/shellapi.go
@@ -5,4 +5,13 @@ func ShellapiHooks(emu *WinEmulator) {
 		Parameters: []string{"a:pszPath", "dwFileAttributes", "psfi", "cbFileInfo", "uFlags"},
 		Fn:         SkipFunctionStdCall(false, 0),
 	})
+	emu.AddHook("", "FindExecutableA", &Hook{
+		Parameters: []string{"a:lpFile", "a:lpDirectory", "a:lpResult"},
+		Fn: func(emulator *WinEmulator, in *Instruction) bool {
+			result := "C:\\WINDOWS\\system32\\LaunchWinApp.exe"
+			emu.Uc.MemWrite(in.Args[2], append([]byte(result), 0))
+			return SkipFunctionStdCall(true, 33)(emu, in)
+		},
+	})
+
 }
